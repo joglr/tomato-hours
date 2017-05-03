b0f72a8a8274f4f760c560c86a2b7e65b631771b
Jonas Røssum <Jonas Rssum>
2017-05-03 21:49:57 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import createClass from 'create-react-class'
+import './App.css'
+
+const App = createClass({
+  render: function() {
+    return (
+      <DefaultView />
+    )
+  }
+})
+
+export default App
