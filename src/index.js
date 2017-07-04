dca7898aba9e2feeb68cdbda7f843aea375ded3a
Jonas Røssum <jonaswehoo@gmail.com>
2017-07-04 20:45:51 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 import './raven'
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 )
