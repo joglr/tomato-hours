c0f2a9c117161eacb730aeb83bc8839be8de6a57
Jonas Røssum <jonaswehoo@gmail.com>
2017-08-05 00:26:13 +0200

    implemented redux in TimeDIsplay moved EarnerSalary rendering into TimeDIsplay

--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import createClass from 'create-react-class'
+import { connect } from 'react-redux'
+import formatTime from './../format-time'
+import calculateEllapsedTime from './../calculate-ellapsed-time'
+import EarnedSalary from './../components/EarnedSalary'
 
-const TimeDisplay = createClass({
-  getDefaultProps: () => ({
-    time: '0s'
-  }),
-  render: function() {
-    return <h1 className="time-display">{this.props.time}</h1>
-  }
-})
+let TimeDisplay = ({ showEarnedSalary, hourlyRate, startTime }) => (
+  <div>
+    <h1 className="time-display">{ startTime ? formatTime(calculateEllapsedTime(startTime, new Date())) : "0s"}</h1>
+    { showEarnedSalary && hourlyRate && startTime
+      ? <EarnedSalary />
+      : [] }
+  </div>
+)
+
+const mapStateToProps = ({
+  settings: { showEarnedSalary, hourlyRate },
+  timer: { currentSession: { startTime } }
+}) => {
+  return { showEarnedSalary, hourlyRate, startTime }
+}
+
+TimeDisplay = connect(mapStateToProps)(TimeDisplay)
 
 export default TimeDisplay
