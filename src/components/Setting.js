8939b385c920412e42bc4a16cd230c61ee868a3b
Jonas Røssum <jonaswehoo@gmail.com>
2017-08-05 00:25:05 +0200

    rewrote Setting

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,41 +1,27 @@
 import React from 'react'
-import createClass from 'create-react-class'
 import PropTypes from 'prop-types'
-
 import ListItemControl from 'react-md/lib/Lists/ListItemControl'
 import Switch from 'react-md/lib/SelectionControls/Switch'
 
-const Setting = createClass({
-  render: function() {
-    const {
-      setting,
-      settings,
-      label,
-      callback
-    } = this.props
-    return <ListItemControl
-      secondaryAction={
-        <Switch
-          id={ setting }
-          name={ setting }
-          label={ label }
-          labelBefore
-          onChange={ value => callback(setting, value) }
-          { ... settings[setting]
-            ? { defaultChecked: true }
-            : {}
-          }
-        />
-      }
-    />
-  }
-})
+const Setting = ({ value, label, onChange }) => {
+  const id = Math.random()
+  return <ListItemControl
+    secondaryAction={
+      <Switch
+        id={ id }
+        name={ id }
+        label={ label }
+        onChange={ onChange }
+        defaultChecked={ value }
+      />
+    }
+  />
+}
 
 Setting.propTypes = {
-  setting: PropTypes.string.isRequired,
-  settings: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
-  callback: PropTypes.func.isRequired
 }
 
 export default Setting
