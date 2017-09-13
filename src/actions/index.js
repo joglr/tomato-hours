616e5acacf0c34c6f488a28dc238f535da1db292
Jonas Røssum <jonaswehoo@gmail.com>
2017-09-13 09:59:11 +0200

    refactoring and imlementation of redux-timer-middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,38 +1,33 @@
+import { START_TIMER, STOP_TIMER } from 'redux-timer-middleware'
+
 /*
  * Sessions
  */
 
 // Action types
+export const TICK = 'TOMATO_TICK'
+export const TIMER_END = 'TOMATO_TICK_END'
 
-export const TOGGLE_TIMER = 'TOGGLE_TIMER_PAUSE'
+// Other constants
+export const TIMER_NAME = 'TOMATO_TIMER'
 
 // Action creators
-export const toggleTimer = () => ({ type: TOGGLE_TIMER })
+export const startTimer = () => ({ type: START_TIMER, payload: { actionName: TICK, timerName: TIMER_NAME }})
+export const stopTimer = () => ({ type: STOP_TIMER, payload: { timerName: TIMER_NAME }})
 
 /*
  * Settings
  */
 
 // Action types
-
 export const TOGGLE_EARNED_SALARY_VISIBILITY = 'TOGGLE_EARNED_SALARY_VISIBILITY'
-export const SET_HOURLY_RATE = 'SET_SALARY'
+export const SET_HOURLY_RATE = 'SET_HOURLY_RATE'
 export const TOGGLE_REMEMBER_SETTINGS = 'REMEMBER_SETTINGS'
 
 // Action creators
-
-export const setShowEarnedSalary = value => ({
-  type: TOGGLE_EARNED_SALARY_VISIBILITY,
-  value
-})
-export const setHourlyRate = value => ({
-  type: SET_HOURLY_RATE,
-  value
-})
-export const setRememberSettings = value => ({
-  type: TOGGLE_REMEMBER_SETTINGS,
-  value
-})
+export const setShowEarnedSalary = value => ({ type: TOGGLE_EARNED_SALARY_VISIBILITY, value })
+export const setHourlyRate = value => ({ type: SET_HOURLY_RATE, value })
+export const setRememberSettings = value => ({ type: TOGGLE_REMEMBER_SETTINGS, value })
 
 export const settingToggleActionCreators = {
   setShowEarnedSalary,
