17e327b4136ec6fed42ea588d63e6bbd4672a1ca
Jonas Røssum <jonaswehoo@gmail.com>
2018-01-21 23:26:38 +0100

    fixed TimeDisplay not updating made EarnedSalary use ellapsedTime from store as well cause: should use ellapsedTime from store

--- src/components/EarnedSalary.js
+++ src/components/EarnedSalary.js
@@ -3,17 +3,16 @@ import { connect } from 'react-redux'
 import getEllapsedTime from './../get-ellapsed-time'
 
 let EarnedSalary = ({ earnedSalary }) => {
-  console.log(earnedSalary)
   return <div style={{ padding: "1rem" }}>
     Earned salary: <span style={{ fontWeight: "bold" }}>{ earnedSalary }</span>
   </div>
 }
 
 const mapStateToProps = ({
-  timer: { currentSession: { startTime } },
+  timer: { currentSession: { ellapsedTime } },
   settings: { hourlyRate }
 }) => ({
-  earnedSalary: Math.round((getEllapsedTime(startTime, new Date()) / 3600) * hourlyRate || 0)
+  earnedSalary: Math.round(ellapsedTime / 3600 * hourlyRate || 0)
 })
 
 EarnedSalary = connect(mapStateToProps)(EarnedSalary)
--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import formatTime from './../format-time'
-import getEllapsedTime from './../get-ellapsed-time'
 import EarnedSalary from './../components/EarnedSalary'
 
-let TimeDisplay = ({ showEarnedSalary, hourlyRate, startTime }) => (
+let TimeDisplay = ({ showEarnedSalary, hourlyRate, ellapsedTime }) => (
   <div>
-    <h1 className="time-display">{ startTime ? formatTime(getEllapsedTime(startTime, new Date())) : "0s"}</h1>
-    { showEarnedSalary && hourlyRate && startTime
+    <h1 className="time-display">{ formatTime(ellapsedTime) || "0s" }</h1>
+    { showEarnedSalary && hourlyRate
       ? <EarnedSalary />
       : [] }
   </div>
 )
 
 const mapStateToProps = ({
-  settings: { showEarnedSalary, hourlyRate },
-  timer: { currentSession: { startTime } }
+  timer: { currentSession: { ellapsedTime } },
+  settings: { showEarnedSalary, hourlyRate }
 }) => {
-  return { showEarnedSalary, hourlyRate, startTime }
+  return { showEarnedSalary, hourlyRate, ellapsedTime }
 }
 
 TimeDisplay = connect(mapStateToProps)(TimeDisplay)
