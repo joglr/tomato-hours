7c83be4272d458f36265ea767ce9b94b08c5922c
Jonas Røssum <jonaswehoo@gmail.com>
2018-01-25 22:58:42 +0100

    enabled sentry-io crash reporting via raven-js

--- src/crash-reporter.js
+++ src/crash-reporter.js
@@ -1,14 +1,21 @@
+import Raven from 'raven-js'
+
+Raven
+  .config('https://37814d2ed7654802807f3f1eb88446bc@sentry.io/178156')
+  .install()
+
 const crashReporter = store => next => action => {
   try {
     return next(action)
   } catch (err) {
-    console.error('Caught an exception!', err)
-    // Raven.captureException(err, {
-    //   extra: {
-    //     action,
-    //     state: store.getState()
-    //   }
-    // })
+    if (process.env.NODE_ENV === 'production') {
+      Raven.captureException(err, {
+        extra: { action, state: store.getState() }
+      })
+    } else if (process.env.NODE_ENV === 'development') {
+      console.info('Exception caught')
+      console.error(err)
+    }
     throw err
   }
 }
