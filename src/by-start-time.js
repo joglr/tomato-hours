13abb7b07d41ccd43d0c29f624572bb8b48b61cd
Jonas Røssum <jonaswehoo@gmail.com>
2018-02-12 11:56:36 +0100

    updated test for by-start-time to reflect new changes fixed by-start-time

--- src/by-start-time.js
+++ src/by-start-time.js
@@ -1,4 +1,4 @@
 export default reversed => (a, b) => {
-  const [aT, bT] = [a, b].map(x => x.startTime.getTime())
+  const [aT, bT] = [a, b].map(x => x.parts[0].startTime.getTime())
   return reversed ? aT > bT : bT > aT
 }
