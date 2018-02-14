9523ecf98f0ef2d3bf70f68af31570bbb3661b0d
Jonas Røssum <jonaswehoo@gmail.com>
2018-02-14 16:10:36 +0100

    reformatted SessionsList

--- src/components/SessionsList.js
+++ src/components/SessionsList.js
@@ -8,8 +8,8 @@ import List from 'react-md/lib/Lists/List'
 import Subheader from 'react-md/lib/Subheaders'
 import ListItem from 'react-md/lib/Lists/ListItem'
 import FontIcon from 'react-md/lib/FontIcons'
+import Avatar from 'react-md/lib/Avatars/Avatar'
 
-const TimeIcon = () => <FontIcon>timer</FontIcon>
 const TrashIcon = () => <FontIcon>delete</FontIcon>
 
 let SessionsList = ({ sessions }) => (
@@ -18,17 +18,36 @@ let SessionsList = ({ sessions }) => (
     { sessions.length > 0
       ? sessions
         .sort(byStartTime())
-        .map(({ title, startTime, stopTime, parts }, key) => <ListItem
+        .map(({
+          title,
+          startTime,
+          stopTime,
+          parts
+        }, key) => <ListItem
+          threeLines
           key={key}
-          leftIcon={<TimeIcon />}
           rightIcon={<TrashIcon />}
+          leftAvatar={<Avatar suffix="red">{
+            title.length > 0
+              ? title.slice(0, 1).toUpperCase()
+              : "U"
+          }</Avatar>}
           primaryText={title.length === 0 ? "Untitled" : title }
-          secondaryText={[ parts && parts[0] && parts[0].startTime && parts[0].startTime.constructor === Date
-            ? moment(parts[0].startTime.getTime()).format('lll')
-            : "Never", formatTime(reduceEllapsedTime(parts))].join(', ')
+          secondaryText={[
+            parts && parts[0] && parts[0].startTime && parts[0].startTime.constructor === Date
+              ? moment(parts[0].startTime.getTime()).format('lll')
+              : "Never",
+            formatTime(reduceEllapsedTime(parts))
+            ].join("\n")
           } />
       )
-      : <ListItem primaryText="No sessions yet" disabled={true} style={{ textAlign: "center" }}/>
+      : <ListItem
+        primaryText="No sessions yet. Why not start one?"
+        disabled={true}
+        style={{
+          textAlign: "center",
+          fontWeight: "bold"
+        }} />
     }
   </List>
 )
@@ -37,6 +56,4 @@ const mapStateToProps = ({ timer: { sessions }}) => ({
   sessions
 })
 
-SessionsList = connect(mapStateToProps)(SessionsList)
-
-export default SessionsList
+export default connect(mapStateToProps)(SessionsList)
