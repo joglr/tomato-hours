ba0314a2603950d7d54beb05153684fc5c44dc80
Jonas Røssum <jonaswehoo@gmail.com>
2018-02-14 16:21:11 +0100

    added setSessionTitle action creator

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -4,7 +4,8 @@ import {
   SESSION_TICK,
   PAUSE_SESSION,
   UNPAUSE_SESSION,
-  END_SESSION
+  END_SESSION,
+  SET_SESSION_TITLE
 } from './../actions'
 
 export const defaultTimer = {
@@ -20,54 +21,23 @@ export const defaultTimer = {
 export default (state = defaultTimer, action) => {
   switch (action.type) {
     // Should add a new part to the current session with a startTime: new Date()
-    case UNPAUSE_SESSION: // AKA START_SESSION
-      const startTime = state.currentSession.parts.length === 0 && state.sessions.length === 0
-        ? window.location.hash.split('#').length === 2
-          ? new Date(getTimeFromHoursAndMinutes(window.location.hash.split('#')[1]))
-          : new Date()
-        : new Date()
-      return {
-          ...state,
-          currentSession: {
-            // Temporary method of inputing startTime
-            ...defaultTimer.currentSession,
-            parts: state.currentSession.parts,
-            startTime,
-            ellapsedTime: reduceEllapsedTime(state.currentSession.parts)
-          }
-        }
-    case SESSION_TICK:
+    case SET_SESSION_TITLE: {
       return {
         ...state,
         currentSession: {
           ...state.currentSession,
-          // Re-calculate ellapsedTime
-          ellapsedTime: reduceEllapsedTime([
-            ...state.currentSession.parts,
-            {
-              startTime: state.currentSession.startTime,
-              stopTime: new Date()
-            }
-          ])
+          title: action.payload.value
         }
       }
-    // Should move the currentSession to a new a new session entry
-    case END_SESSION:
-      return {
-        ...state,
-        ...defaultTimer,
-        sessions: [
-          ...state.sessions,
-          { ...state.currentSession }
-        ]
-      }
+    }
     // Should reset currentSession and move startTime to a new sessionPart with a stopTime
-    case PAUSE_SESSION:
+    case PAUSE_SESSION: {
       return {
         ...state,
         // Reset currenSession to default
         currentSession: {
           ...defaultTimer.currentSession,
+          title: state.currentSession.title,
           // Temporarily display current ellapsedTime until a new one is calculated
           ellapsedTime: state.currentSession.ellapsedTime,
           parts: [
@@ -84,7 +54,57 @@ export default (state = defaultTimer, action) => {
           ]
         },
       }
-    default:
+    }
+    case UNPAUSE_SESSION: {// AKA START_SESSION
+      const startTime = state.currentSession.parts.length === 0 && state.sessions.length === 0
+      ? window.location.hash.split('#').length === 2
+          ? new Date(getTimeFromHoursAndMinutes(window.location.hash.split('#')[1]))
+          : new Date()
+        : new Date()
+      return {
+        ...state,
+        currentSession: {
+          // Temporary method of inputing startTime
+          ...state.currentSession,
+          parts: state.currentSession.parts,
+          startTime,
+          ellapsedTime: reduceEllapsedTime(state.currentSession.parts)
+        }
+      }
+    }
+    case SESSION_TICK: {
+      return {
+        ...state,
+        currentSession: {
+          ...state.currentSession,
+          // Re-calculate ellapsedTime
+          ellapsedTime: reduceEllapsedTime([
+            ...state.currentSession.parts,
+            {
+              startTime: state.currentSession.startTime,
+              stopTime: new Date()
+            }
+          ])
+        }
+      }
+    }
+    // Should move the currentSession to a new a new session entry
+    case END_SESSION: {
+      return {
+        ...state,
+        ...defaultTimer,
+        currentSession: defaultTimer.currentSession,
+        sessions: [
+          ...state.sessions,
+          {
+            ...state.currentSession,
+            title: state.currentSession.title.trim()
+          }
+        ]
+      }
+    }
+    default: {
       return state
+    }
   }
 }
