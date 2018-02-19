3d5b6bbef229ff0366580e96cc2bbec339a05b49
Jonas Røssum <jonaswehoo@gmail.com>
2018-02-19 18:59:21 +0100

    restructured actions

---
+++ src/actions/settings.js
@@ -0,0 +1,18 @@
+// Action types
+export const TOGGLE_EARNED_SALARY_VISIBILITY = 'TOGGLE_EARNED_SALARY_VISIBILITY'
+export const SET_HOURLY_RATE = 'SET_HOURLY_RATE'
+export const TOGGLE_REMEMBER_SETTINGS = 'REMEMBER_SETTINGS'
+export const TOGGLE_NOTIFICATIONS = 'TOGGLE_NOTIFICATIONS'
+
+// Action creators
+export const setShowEarnedSalary = value => ({ type: TOGGLE_EARNED_SALARY_VISIBILITY, payload: { value } })
+export const setHourlyRate = value => ({ type: SET_HOURLY_RATE, payload: { value } })
+export const setRememberSettings = value => ({ type: TOGGLE_REMEMBER_SETTINGS, payload: { value } })
+export const setEnableNotifications = value => ({ type: TOGGLE_NOTIFICATIONS, payload: { value } })
+
+export const settingToggleActionCreators = {
+	setShowEarnedSalary,
+	setHourlyRate,
+	setRememberSettings,
+	setEnableNotifications
+}
