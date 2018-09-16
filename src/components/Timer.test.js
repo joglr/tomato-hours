2602c5a704fd05b06f70ab5233731d08d0e64bc8
Jonas Røssum <jonaswehoo@gmail.com>
2018-09-16 20:03:37 +0200

    fixed component tests

--- src/components/Timer.test.js
+++ src/components/Timer.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Timer from './Timer'
 import { Provider } from 'react-redux'
-import fakeStore from './../fake-store'
+import fakeStore from './../helpers/fake-store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
